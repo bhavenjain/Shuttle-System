395ae0f912083f9d885d7a289b01ac96aca0fd00
keshav251 <keshavkhandelwal251@gmail.com>
2022-01-02 15:42:47 +0530

    models edit

--- backend/models/BusInfo.js
+++ backend/models/BusInfo.js
@@ -2,10 +2,6 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const bus = new Schema({
-  id: {
-    type: Number,
-    required: false
-  },
   from: {
     type: String,
     required: true
