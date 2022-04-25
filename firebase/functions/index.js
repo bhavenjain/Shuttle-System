4e7fb9ca663e07a32052f20665d51b98769f3e5b
keshav251 <keshavkhandelwal251@gmail.com>
2022-04-25 13:32:50 +0530

    firebase

---
+++ firebase/functions/index.js
@@ -0,0 +1,11 @@
+const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp();
+const region = 'asia-south1';
+const functions_reg = functions.region(region);
+const db = admin.firestore();
+
+exports.check = functions_reg.https.onRequest((req, res) =>{
+    res.status(200).json({"status":"working"});
+})
+
