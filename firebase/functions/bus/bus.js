def4e5938b32f1ff308e58ededaf15f4a59d77d0
keshav251 <keshavkhandelwal251@gmail.com>
2022-04-27 17:52:37 +0530

    bus

--- firebase/functions/bus/bus.js
+++ firebase/functions/bus/bus.js
@@ -20,13 +20,27 @@ exports.addBus = functions_reg.https.onRequest(async (req,res) =>{
 
 });
 
-module.exports.getBuses = async (req, res) => {
-    try {
-      filter = {}
-      const bus_response = await Bus.find(filter)
-      res.status(200).json({ data: bus_response })
+exports.getBuses = functions_reg.https.onRequest( async (req, res) => {
+  const date = req.query.date;
+  let dates = date.split("/");
+  let n_date = `${dates[2]}-${dates[1]}-${dates[0]}`;
+  let curr_time = Date.now();
+  let end_time = new Date(`${n_date}T23:59:59+0530`);
+  let selected_date = new Date(`${n_date}T00:00:00+0530`);
+  let start_time = selected_date>curr_time?selected_date.getTime():curr_time;
+  try {
+      const query_result = await db_ref_b.where("date",">=",start_time).where("date","<=",end_time.getTime()).orderBy("date","asc").get();
+      let buses =[];
+      if(query_result.size == 0){
+        res.status(200).json({"status":1,"msg":"no buses found"});
+        return;
+      }
+      query_result.forEach(doc => {
+        buses.push(doc.data());    
+      });
+      res.status(200).json({"status":1 ,"data": buses });
     } catch (err) {
       console.log(err)
       res.status(400).json({ msg: 'internal error while fetching busses' })
     }
-  }
+  });
