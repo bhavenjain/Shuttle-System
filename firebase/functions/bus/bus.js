9291986db010274c9ee4d98696252da8c65d1f4a
keshav251 <keshavkhandelwal251@gmail.com>
2022-04-27 18:20:31 +0530

    bus

--- firebase/functions/bus/bus.js
+++ firebase/functions/bus/bus.js
@@ -9,7 +9,11 @@ db_ref_b = db.collection('buses');
 exports.addBus = functions_reg.https.onRequest(async (req,res) =>{
     try{
         const bus = req.body.bus;
+        let dates = bus.date.split("/");
+        let n_date = `${dates[2]}-${dates[1]}-${dates[0]}`;
+        let time_in_milli = new Date(`${n_date}T${bus.time}:00+0530`)
         bus["uid"] = db_ref_b.doc().id;
+        bus["time_in_milli"] = time_in_milli.getTime(); 
         await db_ref_b.doc().set(bus);
         res.status(200).json({"status":1,"msg":"Bus aded successfully"});
 
@@ -28,8 +32,9 @@ exports.getBuses = functions_reg.https.onRequest( async (req, res) => {
   let end_time = new Date(`${n_date}T23:59:59+0530`);
   let selected_date = new Date(`${n_date}T00:00:00+0530`);
   let start_time = selected_date>curr_time?selected_date.getTime():curr_time;
+  console.log(n_date)
   try {
-      const query_result = await db_ref_b.where("date",">=",start_time).where("date","<=",end_time.getTime()).orderBy("date","asc").get();
+      const query_result = await db_ref_b.where("time_in_milli",">=",start_time).where("time_in_milli","<=",end_time.getTime()).orderBy("time_in_milli","asc").get();
       let buses =[];
       if(query_result.size == 0){
         res.status(200).json({"status":1,"msg":"no buses found"});
